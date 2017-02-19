2ef90e1cb5c214b8857c716168f1876564988494
HomuHomu <ecoco666@gmail.com>
2017-02-19 11:38:46 +0900

    changed celoxdcm vendor
    Change-Id: Iad3d570b4c96f2881580e3fb1a394028f28781a0

--- celoxdcm-vendor.mk
+++ celoxdcm-vendor.mk
@@ -14,4 +14,4 @@
 
 # Pick up overlay for features that depend on non-open-source files
 
-$(call inherit-product, vendor/samsung/celox/celox-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/celoxdcm/celoxdcm-vendor-blobs.mk)
